08e59da8860119e430749632d8c87973579d1970
liu <2327298708@qq.com>
2020-03-20 23:13:02 +0800

    Signed-off-by: liu <2327298708@qq.com> xiaotang

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+ select top 3 name,sex,age,bloodName from dbo.stuInfo inner join dbo.blood
+  on stuInfo.bloodId=blood.bloodId
+  select top 3 name,star.starName,sex,age from dbo.stuInfo inner join dbo.star
+  on stuInfo.starId=star.starId
+  select top 3 name,sex,age,star.starName,dbo.blood.bloodName from  dbo.stuInfo left join dbo.blood 
+  on stuInfo.bloodId=dbo.blood.bloodId
+  left join dbo.star on  dbo.stuInfo.starId=dbo.star.starId
+  
